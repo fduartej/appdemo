cf1530b1af25ce129be4850bea81494d298789ad
Fred Duarte <fduartej@gmail.com>
2022-03-05 13:07:32 -0500

    Repositorio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using appdemo.Models;
+
+namespace appdemo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+   public IActionResult Consultar(Persona p)
+    {
+        ViewData["Message"] = "Hola soy un action "+p.Dni;
+        return View("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
